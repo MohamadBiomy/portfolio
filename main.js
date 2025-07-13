ebdd1b862f6c80b8656a1f735857c7a617cba402
Mohamad Biomy <me.mo.75.20.09.biomy@gmail.com>
2025-07-14 02:18:02 +0300

    about section

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // DOM elements 
 const logo = document.querySelector("header .logo p")
+const sections = document.querySelectorAll("section:not(#hero)")
+// const oLetters = document.querySelectorAll("h2 span")
 
 
 
@@ -8,3 +10,29 @@ const logo = document.querySelector("header .logo p")
 logo.addEventListener("click", () => {
   window.scrollTo(0, 0)
 })
+
+// Add animations to sections
+sections.forEach(section => {
+  section.style.opacity = "0"
+  window.addEventListener('scroll', () => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const triggerPoint = window.innerHeight - 10; // adjust if needed
+
+    if (sectionTop < triggerPoint) {
+      section.style.animation = "appear-from-bottom .5s forwards"
+    }
+  });
+})
+
+// O letters
+// oLetters.forEach(o => {
+//   o.style.position = "relative"
+//   o.parentElement.addEventListener("mouseenter", () => {
+//     o.style.position = "static"
+//   })
+//   o.parentElement.addEventListener("mouseleave", () => {
+//     setTimeout(() => {
+//       o.style.position = "relative"
+//     }, 400)
+//   })
+// })
