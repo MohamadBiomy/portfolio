40ea529ad195ba90bfe2573fa07294a1087475f3
Mohamad Biomy <me.mo.75.20.09.biomy@gmail.com>
2025-07-15 07:39:01 +0300

    projects section

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const logo = document.querySelector("header .logo p")
 const animatedElements = document.querySelectorAll(".animated")
 const navigators = document.querySelectorAll("nav a")
 const sections = document.querySelectorAll("section")
-
+const projectsContainer = document.querySelector("#projects .projects-container")
 
 // Logo
 logo.addEventListener("click", () => {
@@ -41,3 +41,46 @@ navigators.forEach(link => {
 
   })
 })
+
+// Projects
+fetch("./data/projects.json").then(res => res.json())
+.then(data => {
+  data.forEach(project => {
+    const projectElement = createProjectElement(project)
+    projectsContainer.append(projectElement)
+  })
+})
+
+
+
+/* --------- FUNCTIONS --------- */
+
+function createProjectElement(data) {
+  const project = document.createElement("div");
+  project.className = "project";
+  const projectCover = document.createElement("div");
+  projectCover.className = "project-cover";
+  project.append(projectCover)
+  const caption = document.createElement("h5")
+  caption.innerHTML = data.caption
+  projectCover.append(caption)
+  projectCover.style.backgroundImage = `url(${data.image})`
+  console.log(data.image)
+  const description = document.createElement("p")
+  description.innerHTML = data.description
+  project.append(description)
+  const bottom = document.createElement("div")
+  bottom.className = "bottom"
+  project.append(bottom)
+  const repo = document.createElement("div")
+  repo.className = "repo"
+  repo.innerHTML = `<i class="fa-brands fa-github"></i> Github Repo`
+  repo.addEventListener("click", () => window.open(data.repo, "_blank"))
+  bottom.append(repo)
+  const liveSite = document.createElement("div")
+  liveSite.className = "live-site"
+  liveSite.innerHTML = `Live site <i class="fa-solid fa-arrow-right"></i>`
+  liveSite.addEventListener("click", () => window.open(data.liveSite, "_blank"))
+  bottom.append(liveSite)
+  return project
+}
--- toCheck.md
+++ toCheck.md
@@ -1,2 +1,8 @@
+# to discuss
 - color #
 - hello stroke #
+- projects section shape
+
+# projects to be responsive 
+- manage 
+- bookmark 
