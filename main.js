63d9492e4bebac8ed76e3fabf1b56b174b9d289e
Mohamad Biomy <me.mo.75.20.09.biomy@gmail.com>
2025-07-15 03:04:24 +0300

    qualities section

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 // DOM elements 
 const logo = document.querySelector("header .logo p")
-const sections = document.querySelectorAll("section:not(#hero)")
-// const oLetters = document.querySelectorAll("h2 span")
-
-
+const animatedElements = document.querySelectorAll(".animated")
+const navigators = document.querySelectorAll("nav a")
+const sections = document.querySelectorAll("section")
 
 
 // Logo
@@ -11,28 +10,34 @@ logo.addEventListener("click", () => {
   window.scrollTo(0, 0)
 })
 
-// Add animations to sections
-sections.forEach(section => {
+// Add animations to animated elements
+animatedElements.forEach(section => {
   section.style.opacity = "0"
   window.addEventListener('scroll', () => {
     const sectionTop = section.getBoundingClientRect().top;
     const triggerPoint = window.innerHeight - 10; // adjust if needed
-
     if (sectionTop < triggerPoint) {
       section.style.animation = "appear-from-bottom .5s forwards"
     }
   });
+  window.addEventListener("load", () => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const triggerPoint = window.innerHeight - 10; // adjust if needed
+    if (sectionTop < triggerPoint) {
+      section.style.animation = "appear-from-bottom .5s forwards"
+    }
+  })
 })
 
-// O letters
-// oLetters.forEach(o => {
-//   o.style.position = "relative"
-//   o.parentElement.addEventListener("mouseenter", () => {
-//     o.style.position = "static"
-//   })
-//   o.parentElement.addEventListener("mouseleave", () => {
-//     setTimeout(() => {
-//       o.style.position = "relative"
-//     }, 400)
-//   })
-// })
+// Navigators
+navigators.forEach(link => {
+  link.addEventListener("click", () => {
+    
+    const choosedSection = document.getElementById(link.dataset.link)
+
+    if (choosedSection) {
+      window.scrollTo(0, choosedSection.offsetTop - 60)
+    }
+
+  })
+})
